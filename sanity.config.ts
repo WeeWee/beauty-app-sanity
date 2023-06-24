e3401e7839440a61ff8e58f8303fef3480da3747
IndieAK <kindbergadam@gmail.com>
2023-06-24 13:53:34 +0200

    .env

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,11 +4,11 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
-  name: 'Beauty Cosmetics',
-  title: 'beauty-ecommerce',
+  name: 'beauty-ecommerce',
+  title: 'Beauty Cosmetics',
   basePath: '/admin/sanity',
-  projectId: 'qoxzzxih',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
 
   plugins: [deskTool(), visionTool()],
 
